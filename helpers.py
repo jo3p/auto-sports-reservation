a15666b65c4c6a3070cabf26ee495773f173aa35
jo3p <joep96@hotmail.com>
2021-01-27 20:31:21 +0100

    Implement context manager for webdriver to ensure graceful exit

---
+++ helpers.py
@@ -0,0 +1,9 @@
+class WebDriver:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def __enter__(self):
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.quit()
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from seleniumrequests import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from helpers import WebDriver
 
 if platform.system() == 'Linux':
     chrome_options = Options()
@@ -46,60 +47,55 @@ if sport == 'tennis':
         postponed = False
 
     # opening a chrome session and navigating to the login page
-    driver = Chrome(options=chrome_options)
-    driver.get('https://dmsonline.uvt.nl/en/auth/connect_uvt')
-    time.sleep(3)
+    with WebDriver(Chrome(options=chrome_options)) as driver:
+        driver.get('https://dmsonline.uvt.nl/en/auth/connect_uvt')
+        time.sleep(3)
 
-    # search for the right University to be redirected to the logon page
-    search_bar = driver.find_element_by_css_selector('.mod-search-input')
-    search_bar.click()
-    search_bar.send_keys('Tilburg')
-    search_bar.send_keys(Keys.RETURN)
-    time.sleep(3)
+        # search for the right University to be redirected to the logon page
+        search_bar = driver.find_element_by_css_selector('.mod-search-input')
+        search_bar.click()
+        search_bar.send_keys('Tilburg')
+        search_bar.send_keys(Keys.RETURN)
+        time.sleep(3)
 
-    # fill credentials
-    username_field = driver.find_element_by_id('username')
-    username_field.click()
-    username_field.clear()
-    username_field.send_keys(username)
-    time.sleep(1)
-    password_field = driver.find_element_by_id('password')
-    password_field.click()
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.RETURN)
-    time.sleep(1)
+        # fill credentials
+        username_field = driver.find_element_by_id('username')
+        username_field.click()
+        username_field.clear()
+        username_field.send_keys(username)
+        time.sleep(1)
+        password_field = driver.find_element_by_id('password')
+        password_field.click()
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.RETURN)
+        time.sleep(1)
 
-    # navigating to the new reservation page
-    driver.get('https://dmsonline.uvt.nl/en/bookings/view/new')
-    time.sleep(1)
+        # navigating to the new reservation page
+        driver.get('https://dmsonline.uvt.nl/en/bookings/view/new')
+        time.sleep(1)
 
-    # retrieve the cross site reference forgery (csrf) token from the new bookings html page
-    csrf_token = driver.find_element_by_name('csrf_token').get_attribute('value')
+        # retrieve the cross site reference forgery (csrf) token from the new bookings html page
+        csrf_token = driver.find_element_by_name('csrf_token').get_attribute('value')
 
-    # creating the booking POST-method payload
-    booking_payload = {
-        'product_id': '385',
-        'site_id': "0",
-        'csrf_token': csrf_token,
-        'startDate': desired_start,
-        'endDate': desired_end,
-        'maxParticipants': '4',
+        # creating the booking POST-method payload
+        booking_payload = {
+            'product_id': '385',
+            'site_id': "0",
+            'csrf_token': csrf_token,
+            'startDate': desired_start,
+            'endDate': desired_end,
+            'maxParticipants': '4',
 
-    }
+        }
 
-    # if the login was postponed before, execute the actual booking 1sec past midnight
-    if postponed:
-        send_request_datetime = execution_date_datetime + datetime.timedelta(seconds=1)
-        pause.until(send_request_datetime)
+        # if the login was postponed before, execute the actual booking 1sec past midnight
+        if postponed:
+            send_request_datetime = execution_date_datetime + datetime.timedelta(seconds=1)
+            pause.until(send_request_datetime)
 
-    # make actual booking
-    add_booking_url = 'https://dmsonline.uvt.nl/en/bookings/addBooking'
-    driver.request('POST', add_booking_url, data=booking_payload)
-
-    # close chromedriver after execution
-    time.sleep(5)
-    driver.close()
-    driver.quit()
+        # make actual booking
+        add_booking_url = 'https://dmsonline.uvt.nl/en/bookings/addBooking'
+        driver.request('POST', add_booking_url, data=booking_payload)
 
 elif sport == 'crossfit':
     # get user input
@@ -119,47 +115,43 @@ elif sport == 'crossfit':
     else:
         postponed = False
 
-    # retrieve login page
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://crossfitclubpiushaven.sportbitapp.nl/cbm/account/inloggen/")
+    # instantiate WebDriver
+    with WebDriver(webdriver.Chrome(options=chrome_options)) as driver:
+        # retrieve login page
+        driver.get("https://crossfitclubpiushaven.sportbitapp.nl/cbm/account/inloggen/")
 
-    # fill credentials
-    username_elem = driver.find_element_by_name("username")
-    username_elem.clear()
-    username_elem.send_keys(username)
-    time.sleep(1)
-    password_elem = driver.find_element_by_name("password")
-    password_elem.clear()
-    password_elem.send_keys(password)
-    password_elem.send_keys(Keys.RETURN)
-    time.sleep(2)
+        # fill credentials
+        username_elem = driver.find_element_by_name("username")
+        username_elem.clear()
+        username_elem.send_keys(username)
+        time.sleep(1)
+        password_elem = driver.find_element_by_name("password")
+        password_elem.clear()
+        password_elem.send_keys(password)
+        password_elem.send_keys(Keys.RETURN)
+        time.sleep(2)
 
-    # go to the right overview page by weeknumber
-    weeknumber = reservation_date.isocalendar()[1]
-    year = reservation_date.isocalendar()[0]
-    driver.get(
-        f"https://crossfitclubpiushaven.sportbitapp.nl/cbm/account/lesmomenten/?year={year}&week={weeknumber}")
-    time.sleep(1)
+        # go to the right overview page by weeknumber
+        weeknumber = reservation_date.isocalendar()[1]
+        year = reservation_date.isocalendar()[0]
+        driver.get(
+            f"https://crossfitclubpiushaven.sportbitapp.nl/cbm/account/lesmomenten/?year={year}&week={weeknumber}")
+        time.sleep(1)
 
-    # retrieve the sign-on url from the overview webpage
-    lesson_url = driver.find_element_by_css_selector(
-        f'[data-date="{reservation_date_string}"][data-time-start="{reservation_start_time_string}"]').get_attribute(
-        'href')
-    enrol_url = lesson_url + "aanmelden"
-    de_enrol_url = lesson_url + "afmelden"
-    time.sleep(1)
+        # retrieve the sign-on url from the overview webpage
+        lesson_url = driver.find_element_by_css_selector(
+            f'[data-date="{reservation_date_string}"][data-time-start="{reservation_start_time_string}"]') \
+            .get_attribute('href')
+        enrol_url = lesson_url + "aanmelden"
+        de_enrol_url = lesson_url + "afmelden"
+        time.sleep(1)
 
-    # if the login was postponed before, execute the actual booking 1sec past midnight
-    if postponed:
-        send_request_datetime = execution_date_datetime + datetime.timedelta(seconds=1)
-        pause.until(send_request_datetime)
+        # if the login was postponed before, execute the actual booking 1sec past midnight
+        if postponed:
+            send_request_datetime = execution_date_datetime + datetime.timedelta(seconds=1)
+            pause.until(send_request_datetime)
 
-    if de_enrol_bool == 'enrol':
-        driver.get(enrol_url)
-    elif de_enrol_bool == 'de-enrol':
-        driver.get(de_enrol_url)
-
-    # close chromedriver after execution
-    time.sleep(5)
-    driver.close()
-    driver.quit()
+        if de_enrol_bool == 'enrol':
+            driver.get(enrol_url)
+        elif de_enrol_bool == 'de-enrol':
+            driver.get(de_enrol_url)
